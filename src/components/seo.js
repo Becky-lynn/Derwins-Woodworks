08c77e93c51c99e4b10420fd04aaccb9b6fe5dad
Rebecca Derwin <beckylynn9468@yahoo.com>
2021-05-01 04:43:51 -0500

    added meta viewport

--- src/components/seo.js
+++ src/components/seo.js
@@ -29,6 +29,7 @@ const SEO = ({ title, description, image, article }) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta name="google-site-verification" content="I7_RYQonVKUzkHhw5O_XVyroSGTLEdZjOe8mQm3oxzg" />
+      <meta name="viewport" content="width=device-width" />
 
       {seo.url && <meta property="og:url" content="{seo.url}" />}
 
